8b8489394fa5720acbedf20de7a69d240110f980
Aguir-Ichrak <ichrakaguir123@gmail.com>
2026-02-14 18:55:29 +0100

    Configure Redux store with authSlice and appointmentSlice

---
+++ src/services/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import appointmentsReducer from "./slices/appointmentsSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    appointments: appointmentsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
