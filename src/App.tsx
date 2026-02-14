097991791bf356c3ca29b4c36d0bcb98f5007bb7
Aguir-Ichrak <ichrakaguir123@gmail.com>
2026-02-14 18:52:46 +0100

    Add auth state handling and loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider, useSelector } from "react-redux";
+import { NavigationContainer } from "@react-navigation/native";
+import { RootState, store } from "./services/store";
+import AppNavigator from "./navigation/AppNavigator";
+import LoadingScreen from "./screens/LoadingScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const RootApp: React.FC = () => {
+  const { user, loading } = useSelector((state: RootState) => state.auth);
+
+  if (!user) return <LoginScreen />;
+  if (loading) return <LoadingScreen />;
+
+  return <AppNavigator />;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <RootApp />
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
